06c37905433834d85198eb6a1d7d8456146b852f
willeydelavega <107179912+willeydelavega@users.noreply.github.com>
2023-01-19 16:09:23 +0100

    fin

--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -10,15 +10,23 @@ public class ScoreDisplay : MonoBehaviour
 {
     [SerializeField] private TMP_Text _scoreDisplay;
     [SerializeField] IntVariable  _currentScore;
+    private int ScoreUpdate = 0;
 
     void Start()
     {
-        
+         ScoreUpdate= _currentScore.Value; 
+         _scoreDisplay.text = _currentScore.Value.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(_currentScore.Value != ScoreUpdate)
+        {
+           ScoreUpdate= _currentScore.Value; 
+         _scoreDisplay.text = _currentScore.Value.ToString();
+        }
         
+     
     }
 }
